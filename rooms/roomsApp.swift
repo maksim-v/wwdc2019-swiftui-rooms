05dcbe1d533ed02481bcf458726bd3feadc801bc
Maksim Varvashevich <max270071@gmail.com>
2022-04-06 20:42:21 +0300

    Initial Commit

---
+++ rooms/roomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  roomsApp.swift
+//  rooms
+//
+//  Created by Максим Варвашевич on 06.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct roomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
